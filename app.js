88d0c1d580ac2cb87728d5d89ce0d8737f1e1f78
Emily <episaygnane@me.com>
2022-08-05 10:30:10 -0700

    form eventlistener added

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-// import functions and grab DOM elements
 import { renderFighter } from "./render-monster.js";
 const defeatedNumberEl = document.querySelector('#defeated-number');
-const fighter
-// let state
+const fighterHPEl = document.querySelector('#fighter-hp');
+const fighterImgEl = document.querySelector('#fighter-img');
+const form = document.querySelector('form');
+const fighterListEl = document.querySelector('.fighter');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let defeatedFighterCount = 0;
+let playerHP = 15;
+let monster = [
+    { name: 'Tom Nook', hp: 8 },
+    { name: 'Mr. Resetti', hp: 6 },
+];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const monsterName = data.get('monster-name');
+
+    const newMonster = {
+        name: monsterName,
+        hp: Math.ceil(Math.random() * 5),
+    };
+
+    monsters.push(newMonster);
+
+    displayMonsters();
+});
